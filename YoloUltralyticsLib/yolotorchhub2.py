a53275e0620a381b4a6d0b14a8859e0e8033b020
Surfytom <surfytom@gmail.com>
2024-02-09 22:14:41 +0000

    added debug prints with debug global variable. YOLO and image padding now works with multiple images

--- YoloUltralyticsLib/yolotorchhub2.py
+++ YoloUltralyticsLib/yolotorchhub2.py
@@ -2,59 +2,67 @@ import torch
 import cv2
 from ultralytics import YOLO
 import numpy as np
+import timeit
+
+DEBUG = True
 
 def fitToImage(imageShape, xyxy):
 
-    print(imageShape)
+    if DEBUG:
+        print(f"image shape before reverse: {imageShape}")
 
     imageShape = imageShape[::-1]
 
-    print(imageShape)
+    if DEBUG:
+        print(f"image shape after reverse: {imageShape}")
 
-    for i in range(2):
-
-        for j in range(i, 4, 2):
-            #print(f"i: {i} | j: {j}")
-            if xyxy[j] < 0:
-                #print("lesser than 0")
-                xyxy[j] = 0
-            elif xyxy[j] >= imageShape[i]:
-                xyxy[j] = imageShape[i]-1
+    for i, boxes in enumerate(xyxy):
+        for j, box in enumerate(boxes):
+            for t in range(2):
+                for p in range(t, 4, 2):
+                    if DEBUG:
+                        print(f"i: {i} | j: {j} | t: {t} | p: {xyxy[i][j][p]}")
+                    if xyxy[i][j][p] < 0:
+                        xyxy[i][j][p] = 0
+                    elif xyxy[i][j][p] >= imageShape[t]:
+                        xyxy[i][j][p] = imageShape[t]-1
     
-    #print(xyxy)
+    if DEBUG:
+        print(f"boxes after fit to image: {xyxy}")
                 
     return xyxy
 
 def padBox(cords, padding):
 
-    cords[:2] = cords[:2] - padding
-    cords[2:] = cords[2:] + padding
+    for i, boxes in enumerate(cords):
+        for j, box in enumerate(boxes):
+            cords[i][j][:2] = box[:2] - padding
+            cords[i][j][2:] = box[2:] + padding
 
-    print(f"cords after paddings: {cords}")
+    if DEBUG:
+        print(f"boxes after paddings: {cords}")
 
     return cords
 
-
 model = YOLO("yolov8n.pt")
 
-results = model("input_image.jpeg")
+results = model(["input_image.jpeg", "input_image.jpeg"])
 
-print(f"results: {results[0].boxes.xyxy}")
+results = [result.boxes.xyxy.detach().cpu().numpy().astype(np.intp) for result in results]
+
+print(f"results: {results}")
 
 image = cv2.imread("input_image.jpeg")
 
 shape = np.shape(image)
-print(f"shape: {shape}")
 
-cords = results[0].boxes.xyxy[0].numpy().astype(np.intp)
+results = padBox(results, 100)
 
-cords = padBox(cords, 100)
+results = fitToImage(shape[:2], results)
 
-cords = fitToImage(shape[:2], cords)
+x, y, x1, y1 = results[0][0]
 
-x, y, x1, y1 = cords
-
-print(f"x {x} | y {y} | x1 {x1} | y1 {y1}")
+print(f"xyxy of showcase image and first box: x {x} | y {y} | x1 {x1} | y1 {y1}")
 
 cv2.rectangle(image, (x, y), (x1, y1), (255, 0, 0), 5)
 
