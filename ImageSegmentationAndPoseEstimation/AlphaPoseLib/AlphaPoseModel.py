eab63b17be150a964c873c28e1e9ebf87f944389
longlh-non <longlh1809@gmail.com>
2024-05-02 15:34:35 +0000

    Optimize AlphaPose containerization process in Dockerfile

---
+++ ImageSegmentationAndPoseEstimation/AlphaPoseLib/AlphaPoseModel.py
@@ -0,0 +1,57 @@
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+install("docker")
+import docker
+client = docker.from_env()
+
+def InitModel(config):    
+    container = client.containers.run(**config)
+    # Print container ID
+    print("Container ID:", container.id)
+
+    print(container.logs().decode('utf-8'))
+    # Stop the container
+
+    # container.stop()
+    return container
+
+def LoadConfig():
+    # Define the container configuration
+    return {
+        'image': 'zhiawei/alpha:latest',
+        'detach': True,
+        'device_requests': [
+            docker.types.DeviceRequest(count=-1, capabilities=[['gpu']])
+        ],
+        'volumes': {
+            '/home/student/horizon-coding/Swim2DPose/data': {
+                'bind': '/data',
+                'mode': 'rw'
+            }
+        },
+        'stdin_open': True,  # Keep STDIN open even if not attached (-i)
+        'tty': True,  # Allocate a pseudo-TTY (-t)
+    }
+
+def Inference(model, videoNames, stopAfterExecuting=True):
+
+    allKeyPoints = []
+
+    for i, videoName in enumerate(videoNames):
+        # Runs the model on a set of images returning the keypoints the model detects
+        model.exec_run(cmd=f'./build/examples/openpose/openpose.bin --video "/data/{videoName}.mp4" --display 0 --hand --write_json "{videoName}"  --write_video "{videoName}.avi"')
+        subprocess.run(["docker", "cp", f"{model.id}:/openpose/{videoName}.avi", f'./OpenPoseLib/results/{videoName}.avi'])
+        subprocess.run(["docker", "cp", f"{model.id}:/openpose/{videoName}", f'./OpenPoseLib/keypoints/{videoName}'])
+    
+    if stopAfterExecuting == True:
+        Stop(model)
+        
+
+    return allKeyPoints
+
+def Stop(model):
+    model.stop()
