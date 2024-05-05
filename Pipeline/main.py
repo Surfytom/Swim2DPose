bc93761732d65516ec55b55b219885f65a5dc979
Surfytom <surfytom@gmail.com>
2024-05-05 16:19:16 +0100

    added relative paths to keypoints json reading and added docker to pipeline docker file

--- Pipeline/main.py
+++ Pipeline/main.py
@@ -107,7 +107,7 @@ if __name__ == "__main__":
 
     if args.model == "DWPose" or args.model == "YoloNasNet":
 
-        with open("keypointGroupings.json", "r") as f:
+        with open(f"{pathlib.Path(__file__).parent.resolve()}/keypointGroupings.json", "r") as f:
             keypointGroups = json.load(f)
 
         selectedPoints = keypointGroups[args.model]
