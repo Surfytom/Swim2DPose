b9e12ae902e916d26e3c6770458b0e5d51ddb0d8
longlh-non <longlh1809@gmail.com>
2024-05-07 16:29:24 +0000

    Change results directory for OpenPose

--- Pipeline/OpenPoseLib/OpenPoseModel.py
+++ Pipeline/OpenPoseLib/OpenPoseModel.py
@@ -1,5 +1,7 @@
 import subprocess
 import docker
+import datetime
+import os
 
 client = docker.from_env()
 client.pipelineContainerId = client.containers.list(filters={"name": "pipeline"})[-1].id
@@ -33,26 +35,35 @@ def LoadConfig(args):
         'device_requests': [
             docker.types.DeviceRequest(count=-1, capabilities=[['gpu']])
         ],
-        'volumes': {
-            f'{args.inputpaths}': {
-                'bind': '/data',
-                'mode': 'rw'
-            }
-        },
+        'volumes_from': [client.pipelineContainerId],
         'stdin_open': True,  # Keep STDIN open even if not attached (-i)
         'tty': True,  # Allocate a pseudo-TTY (-t)
     }
 
-def Inference(model, videoNames, stopAfterExecuting=True):
+def Inference(model, videoNames, folderPath, stopAfterExecuting=True):
 
     allKeyPoints = []
+    if os.path.exists(f'{folderPath}/OpenPose') == False:
+        # Make results folder if it does not exist
+        os.mkdir(f'{folderPath}/OpenPose')
 
-    for i, videoName in enumerate(videoNames):
+    dateTimeF = '{:%d-%m-%Y_%H-%M-%S}'.format(datetime.now())
+
+    if os.path.exists(f'{folderPath}/OpenPose/{dateTimeF}') == False:
+        # Makes a new folder wtih the new highest run digit
+        os.mkdir(f"{folderPath}/OpenPose'/{dateTimeF}")
+
+    for i, video in enumerate(videoNames):
+        videoNameAndExt = video['name'] + video['ext']
+        videoName = video['name']
+        
+        print(f"Video {videoNameAndExt} running through OpenPose")
         # Runs the model on a set of images returning the keypoints the model detects
-        model.exec_run(cmd=f'./build/examples/openpose/openpose.bin --video "/data/{videoName}.mp4" --display 0 --hand --write_json "{videoName}"  --write_video "{videoName}.avi"')
-        subprocess.run(["docker", "cp", f"{model.id}:/openpose/{videoName}.avi", f'./OpenPoseLib/results/{videoName}.avi'])
-        subprocess.run(["docker", "cp", f"{model.id}:/openpose/{videoName}", f'./OpenPoseLib/keypoints/{videoName}'])
-    
+        model.exec_run(cmd=f'./build/examples/openpose/openpose.bin --video "/usr/src/app/media/{videoName}.mp4" --display 0 --hand --write_json "/usr/src/app/media/results/OpenPose/{dateTimeF}/{videoName}"  --write_video "/usr/src/app/media/results/OpenPose/{dateTimeF}/{videoName}.avi"')
+        print(f"Video /usr/src/app/media/results/OpenPose/{dateTimeF}/{videoName}.avi is ready")
+        print(f"Keypoints /usr/src/app/media/results/OpenPose/{dateTimeF}/{videoName}.json is ready")
+
+    # allResultPaths.append(f'/usr/src/app/media/results/AlphaPose/{videosPath}/{videoName}')
     if stopAfterExecuting == True:
         Stop(model)
 
